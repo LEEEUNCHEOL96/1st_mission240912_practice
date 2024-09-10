f718534d25c52ea87eec3bcffc7b538c9ac2f976
LEEEUNCHEOL <dmscjf1020@naver.com>
2024-09-10 19:37:48 +0900

    ãarticleRepository 생성

---
+++ src/main/java/com/example/test240911/Article/ArticleRepository.java
@@ -0,0 +1,7 @@
+package com.example.test240911.Article;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ArticleRepository extends JpaRepository<Article,Integer> {
+}
