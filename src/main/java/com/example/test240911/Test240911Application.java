5f8f7cea4184be3baa5ef136a80406825c8ff6b7
LEEEUNCHOEL <dmscjf1020@gmail.com>
2024-09-10 13:25:19 +0900

    init

---
+++ src/main/java/com/example/test240911/Test240911Application.java
@@ -0,0 +1,13 @@
+package com.example.test240911;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test240911Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Test240911Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/test240911/Test240911ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test240911;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test240911ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
