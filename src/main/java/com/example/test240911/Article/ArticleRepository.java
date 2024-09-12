e1f70f5ba845bd554f28dab2bae3306e18126199
LEEEUNCHEOL <dmscjf1020@naver.com>
2024-09-12 16:08:58 +0900

    article,articleRepositorry 생성

---
+++ src/main/java/com/example/test240911/Article/ArticleRepository.java
@@ -0,0 +1,9 @@
+package com.example.test240911.Article;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ArticleRepository extends JpaRepository<Article,Integer> {
+}
--- src/test/java/com/example/test240911/Test240911ApplicationTests.java
+++ src/test/java/com/example/test240911/Test240911ApplicationTests.java
@@ -1,13 +1,27 @@
 package com.example.test240911;
 
+import com.example.test240911.Article.Article;
+import com.example.test240911.Article.ArticleRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
 
 @SpringBootTest
 class Test240911ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+	private ArticleRepository articleRepository;
 
+	@Test // 테스트용 게시물 생성
+	void contextLoads() {
+		Article article = new Article();
+		article.setTitle("테스트용");
+		article.setContent("지문입니다람쥐");
+
+		article.setCreateDate(LocalDateTime.now());
+		this.articleRepository.save(article);
+
+	}
 }
